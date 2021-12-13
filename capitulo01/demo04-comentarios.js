7bd7707ff23b40e5ac4e12044fd4c93c21e9cbef
Lessa Apolinario <lessaapolinario22@outlook.com>
2021-12-13 13:51:16 -0300

    Comentarios

---
+++ capitulo01/demo04-comentarios.js
@@ -0,0 +1,11 @@
+// comentário em uma única linha
+
+/**
+ * Comentário de múltiplas linhas
+ * Author: Lessa Apolinario
+ * Data: 13/12/2021
+ */
+
+// ignorando a linha abaixo
+// const variavelConstante = 1 + 2 + 4 + 4
+const variavelConstante = 1 + 4 // comentando na linha
