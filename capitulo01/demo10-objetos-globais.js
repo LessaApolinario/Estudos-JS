47ee09652644894ebe477c6ef038929e09f7f439
Lessa Apolinario <lessaapolinario22@outlook.com>
2021-12-15 12:52:23 -0300

    Objetos globais

---
+++ capitulo01/demo10-objetos-globais.js
@@ -0,0 +1,7 @@
+console.log("keys", Object.keys({ nome: "Lessa" }))
+console.log("values", Object.values({ nome: "Lessa" }))
+console.log("now", Date.now())
+console.log("random", Math.random())
+
+// global
+console.log("global", global) // globalThis
