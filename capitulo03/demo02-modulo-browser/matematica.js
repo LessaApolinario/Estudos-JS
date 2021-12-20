d85d84be7001154f50c8cf62089747f3c8bad058
Lessa Apolinario <lessaapolinario22@outlook.com>
2021-12-20 13:59:56 -0300

    Modulos em navegadores

---
+++ capitulo03/demo02-modulo-browser/matematica.js
@@ -0,0 +1,12 @@
+class Matematica {
+  static dividir(valo1, valor2) {
+    return valo1 / valor2
+  }
+
+  static subtrair(valor1, valor2) {
+    return valor1 - valor2
+  }
+}
+
+// já fica público sozinho!
+// só adicionar no HTML
